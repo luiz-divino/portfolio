1d592a610c564bf85ba9d00fee9431a661bb1be4
luiz-divino <nandodivinodev@gmail.com>
2026-07-08 21:50:32 -0300

    feat: create structure initial to navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <nav className="flex justify-between gap-4 p-4 bg-gray-800 text-white">
+      <span>
+        LOGO
+      </span>
+      <div className="flex gap-8 max-w-7xl">
+        <Link href="/">Home</Link>
+        <Link href="/project">Project</Link>
+        <Link href="/about">About</Link>
+        <Link href="/skills">Skills</Link>
+        <Link href="/certifications">Certifications</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+      <div>
+        <button>
+          <Link href="/contact">
+            Vamos Conversar
+          </Link>
+        </button>
+      </div>
+    </nav>
+  );
+}
