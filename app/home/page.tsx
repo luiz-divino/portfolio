bec87f938cf6ce1cae5609cd78803dfe9757523e
luiz-divino <nandodivinodev@gmail.com>
2026-07-10 20:52:03 -0300

    style: adjust component centralization

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,7 +3,7 @@ import { CardTtext } from "@/components/home/card-text";
 export default function Home() {
     return (
       <div className="min-h-screen flex items-center justify-center bg-main-background">
-        <div className="max-w-4xl flex justify-end">
+        <div className="max-w-7xl mx-auto lg:justify-end">
           <CardTtext/>
         </div>
       </div>
