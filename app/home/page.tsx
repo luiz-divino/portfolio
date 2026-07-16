ed45199c22612e41fdc65f486e522279035dcf8d
luiz-divino <nandodivinodev@gmail.com>
2026-07-16 18:35:09 -0300

    refactor: change path file and correct new name file

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,10 @@
-import { CardTtext } from "@/components/home/card-text";
+import { HeroText } from "@/components/home/Herotext";
 
 export default function Home() {
     return (
       <div className="min-h-screen flex items-center justify-center bg-main-background">
         <div className="max-w-7xl mx-auto lg:justify-end">
-          <CardTtext/>
+          <HeroText/>
         </div>
       </div>
     );
